d96b5faa205585374f5d796b0c2153aaf951f528
Paul Maximilian Krüger <paul.krueger@alumni.fh-aachen.de>
2020-12-28 12:23:00 +0100

    changed weekly stat to 19:50

--- src/main/java/de/fhaachen/matse/movebot/control/WeeklyStatManager.kt
+++ src/main/java/de/fhaachen/matse/movebot/control/WeeklyStatManager.kt
@@ -1,6 +1,7 @@
 package de.fhaachen.matse.movebot.control
 
 import de.fhaachen.matse.movebot.POINT_GOAL
+import de.fhaachen.matse.movebot.prettyString
 import de.fhaachen.matse.movebot.round
 import de.fhaachen.matse.movebot.telegram.ChallengeBot
 import de.fhaachen.matse.movebot.telegram.MessageHandler
@@ -17,7 +18,7 @@ import java.time.temporal.TemporalAdjusters
 object WeeklyStatManager {
 
     private var nextWeekReminder = LocalDateTime.now().withHour(12).withMinute(0).with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY))!!
-    private var nextWeekResult: LocalDateTime = LocalDateTime.now().withHour(18).withMinute(0).with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY))!!
+    private var nextWeekResult: LocalDateTime = LocalDateTime.now().withHour(19).withMinute(50).with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY))!!
 
     fun checkGoals() {
         val currentTime = LocalDateTime.now()
@@ -42,8 +43,8 @@ object WeeklyStatManager {
             val message = if (stat.containsChallenger(challenger)) {
                 val place = stat.statEntries.indexOfFirst { it.challenger == challenger } + 1
                 val points = stat.statEntries.first { it.challenger == challenger }.value()
-                "Du hast diese Woche *$points Punkte* erreicht!\nDamit bist du auf *Platz $place* (für diese Woche)."
-            } else "Du hast in der letzten Woche keine Bewegungen erfasst!\nAchtung: Du wirst nicht in der wöchentlichen Statistik um 18:00 auftauchen!\nWillst du das wirklich?"
+                "Du hast diese Woche *$points Punkte* erreicht!\nDamit bist du momentan auf *Platz $place* (für diese Woche)."
+            } else "Du hast in der letzten Woche keine Bewegungen erfasst!\nAchtung: Du wirst nicht in der wöchentlichen Statistik um ${nextWeekResult.toLocalTime().prettyString()} auftauchen!\nWillst du das wirklich?"
 
             try {
                 ChallengeBot.sendMessage(challenger.telegramUser.id, message, keyboard).also { MessageHandler.addDeleteableMessage(it, MessageType.WEEKLY_REMINDER) }
@@ -61,8 +62,8 @@ object WeeklyStatManager {
         val totalPoints = stat.statEntries.sumByDouble { it.value() }.round(0)
 
         val messageOverview = "*Ergebnis der Woche*\n" +
-                "Wir haben gemeinsam *$totalPoints Punkte* erarbeitet.\n" +
-                (if (totalPoints > POINT_GOAL) "\uD83C\uDF89\uD83C\uDF89 *über $POINT_GOAL Punkte in einer Woche!* \uD83C\uDF89\uD83C\uDF89" else "Schaffen wir die $POINT_GOAL nächste Woche?") + "\n" +
+                "Wir haben gemeinsam *$totalPoints Punkte* erreicht.\n" +
+                (if (totalPoints > POINT_GOAL) "\uD83C\uDF89\uD83C\uDF89 *über $POINT_GOAL Punkte in einer Woche!* \uD83C\uDF89\uD83C\uDF89" else "Schaffen wir gemeinsam die $POINT_GOAL nächste Woche?") + "\n" +
                 stat.statEntries.foldIndexed("") { index, old, entry -> "$old\n${index + 1}. *${entry.label}* (${entry.value()} km)" }
 
         ChallengerManager.challengers.forEach { challenger ->
