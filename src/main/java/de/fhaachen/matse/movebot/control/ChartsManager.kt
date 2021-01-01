8366eeacd10458e9450c18310da272fe5964ea9b
Paul Maximilian Krüger <paul.krueger@alumni.fh-aachen.de>
2021-01-01 15:31:02 +0100

    improved ChartsManager.kt

--- src/main/java/de/fhaachen/matse/movebot/control/ChartsManager.kt
+++ src/main/java/de/fhaachen/matse/movebot/control/ChartsManager.kt
@@ -5,6 +5,8 @@ import de.fhaachen.matse.movebot.model.SeriesStatEntry
 import de.fhaachen.matse.movebot.model.Statistic
 import org.knowm.xchart.*
 import org.knowm.xchart.XYSeries.XYSeriesRenderStyle
+import org.knowm.xchart.internal.chartpart.Chart
+import org.knowm.xchart.style.PieStyler
 import org.knowm.xchart.style.Styler.LegendPosition
 import java.awt.Color
 import java.io.File
@@ -12,27 +14,27 @@ import java.io.File
 
 object ChartsManager {
 
-
-    fun getChartPicture(statistic: Statistic): File {
+    fun getChartPicture(statistic: Statistic, asStackedBar: Boolean = false): File {
         val id = statistic.getUniqueName()
         val chartFile = File(getChartFileName(id))
 
         if (chartFile.exists())
             return chartFile
 
-        when {
-            isSeriesStatistic(statistic) -> {
-                val chart: XYChart = generateXYChart(statistic)
-                BitmapEncoder.saveBitmap(chart, getChartFileName(id), BitmapEncoder.BitmapFormat.PNG)
-            }
-            isPieStatistic(statistic) -> {
-                val chart: PieChart = generatePieChart(statistic)
-                BitmapEncoder.saveBitmap(chart, getChartFileName(id), BitmapEncoder.BitmapFormat.PNG)
-            }
+
+        val chart = when {
+            asStackedBar -> generateStackedChart(statistic)
+
+            isSeriesStatistic(statistic) -> generateXYChart(statistic)
+
+            isPieStatistic(statistic) -> generatePieChart(statistic)
+
             else -> {
                 return File("500.png")
             }
         }
+
+        BitmapEncoder.saveBitmapWithDPI(chart, getChartFileName(id), BitmapEncoder.BitmapFormat.PNG, 300)
 
         return chartFile
     }
@@ -46,10 +48,11 @@ object ChartsManager {
     }
 
 
-    private fun getChartFileName(statId: String) = "./chart_${statId.replace(" ", "")}.png"
+    private fun getChartFileName(statId: String) = "./charts/chart_${statId.replace(" ", "")}.png"
 
     private fun generateXYChart(statistic: Statistic): XYChart {
-        val chart: XYChart = XYChartBuilder().width(1000).height(750).title(statistic.name).xAxisTitle("X").yAxisTitle("Y").build()
+        val chart: XYChart =
+            XYChartBuilder().width(1300).height(975).title(statistic.name).xAxisTitle("X").yAxisTitle("Y").build()
 
         // Customize Chart
         chart.styler.legendPosition = LegendPosition.InsideNW
@@ -73,9 +76,32 @@ object ChartsManager {
         chart.styler.chartBackgroundColor = Color.WHITE
         chart.styler.isLegendVisible = true
         chart.styler.legendPosition = LegendPosition.OutsideE
+        chart.styler.annotationType = PieStyler.AnnotationType.Percentage
+        chart.styler.annotationDistance = 0.82
+        chart.styler.defaultSeriesRenderStyle = PieSeries.PieSeriesRenderStyle.Donut
+        chart.styler.seriesColors =
+            arrayOf(Color(1, 74, 127), Color(253, 216, 11), Color(128, 0, 1), Color(165, 254, 4))
 
         statistic.statEntries.filterIsInstance<PointStatEntry>().forEach { entry ->
-            chart.addSeries(entry.label, entry.value)
+            chart.addSeries(entry.label + " (${entry.value} km)", entry.value)
+        }
+
+        return chart
+    }
+
+    private fun generateStackedChart(statistic: Statistic): CategoryChart {
+        val chart: CategoryChart = CategoryChartBuilder().width(1000).height(750).title(statistic.name).build()
+
+        // Customize Chart
+        chart.styler.chartBackgroundColor = Color.WHITE
+        chart.styler.isLegendVisible = true
+        chart.styler.legendPosition = LegendPosition.InsideNW
+        chart.styler.isOverlapped = true
+        chart.styler.seriesColors =
+            arrayOf(Color(1, 74, 127), Color(253, 216, 11), Color(128, 0, 1), Color(165, 254, 4))
+
+        statistic.statEntries.filterIsInstance<SeriesStatEntry>().forEach { entry ->
+            chart.addSeries(entry.label, entry.xData, entry.yData)
         }
 
         return chart
