1085ebc476b911160c86390ae82a34994b3252a5
Paul Maximilian Krüger <paul.krueger@alumni.fh-aachen.de>
2021-01-02 20:54:46 +0100

    updated points

--- src/main/java/de/fhaachen/matse/movebot/MoveBotUtils.kt
+++ src/main/java/de/fhaachen/matse/movebot/MoveBotUtils.kt
@@ -1,7 +1,19 @@
 package de.fhaachen.matse.movebot
 
-val POINT_GOAL = 2021
+const val POINT_GOAL = 2021
 
 fun toFinish(value: Int) = POINT_GOAL.toDouble() / value
 
-fun toBigActivity(value: Double) = 15 / value
+/**
+ * 50_500 Kalorien zum Beenden der Challenge.
+ *
+ * Berechnung:
+ * Es bedarf 50_500 Kalorien zum Beenden der Challenge. Also müssen pro Punkt toFinish(50_500) Kalorien erarbeitet werden.
+ * calPerHour / unitsPerHour = Kalorie pro Einheit
+ * Kalorie pro Einheit / Kalorien pro Punkt = Punkt pro Einheit
+ *
+ * @param calPerHour Int Kalorien pro Stunde
+ * @param unitsPerHour Int Einheiten pro Stunde
+ * @return Double Punkte für eine Einheit
+ */
+fun byCalories(calPerHour: Int, unitsPerHour: Int) = (calPerHour.toDouble() / unitsPerHour) / (50_500.0 / POINT_GOAL)
--- src/main/java/de/fhaachen/matse/movebot/model/MovementType.kt
+++ src/main/java/de/fhaachen/matse/movebot/model/MovementType.kt
@@ -1,22 +1,21 @@
 package de.fhaachen.matse.movebot.model
 
-import de.fhaachen.matse.movebot.toBigActivity
-import de.fhaachen.matse.movebot.toFinish
+import de.fhaachen.matse.movebot.byCalories
 
 enum class MovementType(val title: String, val emoji: String, val unit: String, val pointsPerUnit: Double) {
 
-    BIKING("Radfahren", "\uD83D\uDEB4\u200D♂️", "km", toBigActivity(22.0) ),
-    RUNNING("Laufen", "\uD83C\uDFC3\u200D♂️", "km",  toBigActivity(11.0) ),
-    WALKING("Spazieren", "\uD83D\uDEB6\u200D♂️", "km", toBigActivity(12.0) ),
-    HIKING("Wandern", "⛰", "km", toBigActivity(9.0) ),
-    SWIMMING("Schwimmen", "\uD83C\uDFCA\u200D♂️", "km", toBigActivity(2.0) ),
-    FITNESS ("Fitness", "\uD83C\uDFCB️\u200D♂️", "min", toBigActivity(60.0) ),
-    PUSHUPS ("Liegestütze", "\uD83D\uDE47\uD83C\uDFFC\u200D♂️", "Wdh.", toFinish(15_000)),
-    PULLUPS ("Klimmzüge", "\uD83D\uDCAA", "Wdh.", toFinish(7_500)),
-    SQUATS ("Kniebeugen", "\uD83E\uDDBF", "Wdh.", toFinish(75_000)),
-    PLANKS ("Planks", "\uD83C\uDFF4\u200D☠️", "min", toFinish(3000)),
-    HULAHOOP ("Hula Hoop", "\uD83D\uDD7A", "min", toFinish(14600)),
-    BOULDERING ("Bouldern", "\uD83E\uDDD7", "min", toBigActivity(90.0));
+    BIKING("Radfahren", "\uD83D\uDEB4\u200D♂️", "km", byCalories(450, 27) ), // bisschen schwerer gemacht, die Kalorienzahl sind eigentlich für 22km nicht 27km in einer h
+    RUNNING("Laufen", "\uD83C\uDFC3\u200D♂️", "km",  byCalories(394, 11) ),
+    WALKING("Spazieren", "\uD83D\uDEB6\u200D♂️", "km", byCalories(197, 7) ),
+    HIKING("Wandern", "⛰", "km", byCalories(337, 10) ), // Bisschen schwerer gemacht, 10km/h sind ambitioniert
+    SWIMMING("Schwimmen", "\uD83C\uDFCA\u200D♂️", "km", byCalories(563, 4) ),
+    FITNESS ("Fitness", "\uD83C\uDFCB️\u200D♂️", "min", byCalories(309, 50) ), // Bisschen leichert gemacht, nur 50min in 60min Aktivitätszeit benötigt.
+    PUSHUPS ("Liegestütze", "\uD83D\uDE47\uD83C\uDFFC\u200D♂️", "Wdh.", byCalories(450, 216)), // 30 pro Minute, 12% Aktivitätszeit von 1h
+    PULLUPS ("Klimmzüge", "\uD83D\uDCAA", "Wdh.", byCalories(450, 115)), // 16 pro Minute, 12% Aktivitätszeit von 1h
+    SQUATS ("Kniebeugen", "\uD83E\uDDBF", "Wdh.", byCalories(309, 288)), // 40 pro Minute, 12% Aktivitätszeit von 1h
+    PLANKS ("Planks", "\uD83C\uDFF4\u200D☠️", "min", byCalories(337, 20)), // 33% Aktivitätszeit
+    HULAHOOP ("Hula Hoop", "\uD83D\uDD7A", "min", byCalories(337, 75)), // Bisschen schwerer gemacht, 125% Aktivitätszeit
+    BOULDERING ("Bouldern", "\uD83E\uDDD7", "min", byCalories(450, 75)); // Bisschen schwerer gemacht, 125% Aktivitätszeit
 
     companion object {
         fun of(input: String) = values().single { it.name.equals(input, true) || it.title.equals(input, true) || it.emoji == input }
