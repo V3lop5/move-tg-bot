3e4cb5dd51c08428181978ffed8a5245e6821d66
Paule <44635962+V3lop5@users.noreply.github.com>
2021-12-30 23:37:48 +0100

    Changed message for inYearRequirement

--- src/main/java/de/fhaachen/matse/movebot/telegram/model/BasicRequirements.kt
+++ src/main/java/de/fhaachen/matse/movebot/telegram/model/BasicRequirements.kt
@@ -1,12 +1,13 @@
 package de.fhaachen.matse.movebot.telegram.model
 
+import de.fhaachen.matse.movebot.POINT_GOAL
 import de.fhaachen.matse.movebot.control.ChallengerManager
 import de.fhaachen.matse.movebot.telegram.commands.TutorialCommand
 import java.time.LocalDate
 
 val userchatRequirement = Requirement("Dieser Befehl kann nur im privaten Chat ausgeführt werden.") { _, chat, _ -> chat.isUserChat }
 
-val inYearRequirement = Requirement("Dieser Befehl kann nur im Jahr 2021 genutzt werden. Bitte gedulde dich noch!") { _, _, _ -> LocalDate.now().year == 2021 }
+val inYearRequirement = Requirement("Dieser Befehl kann nur im Jahr $POINT_GOAL genutzt werden. Bitte gedulde dich noch, bis der Bot auf das neue Jahr vorbereitet ist!") { _, _, _ -> LocalDate.now().year == POINT_GOAL }
 
 val allowPersonalShareRequirement = Requirement("Dieser Befehl kann nur genutzt werden, wenn du selber ein Vorstellungsvideo aufgenommen hast und dieses mit den anderen Teilnehmern teilst.\n\nNehme ein Video auf und schicke es mir. Mehr dazu: /tutorial") { user, _, _ -> ChallengerManager.findChallenger(user)?.run { shareVideoAndGoals && isVideoAccepted }?:false }
 
